5d389d4e6992f30c0573eae8088a5c16260b9ec0
Mohammad A Iqbal <nea.testing.coursework@gmail.com>
2022-11-28 18:02:11 +0000

    DOne

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def check_prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                return False
+
+    return True
+
 def primes(number_of_primes):
     list = []
+    while len(list) < number_of_primes:
+        i = 1
+        if check_prime(i):
+            list.append(i)
+        i = i + 1
+        
     return list
